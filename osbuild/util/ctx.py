68bc3f3f660232010100e09f12dcda4bff0fad53
David Rheinsberg <david.rheinsberg@gmail.com>
2020-05-07 18:31:56 +0200

    util/ctx: add guarded context manager
    This adds a new Class `util.ctx.GuardedCM`. This implements a simple
    context-manager that allows any class that inherits `GuardedCM` to
    define `guarded_enter()` and `guarded_exit()` rather than the standard
    `__enter__()` and `__exit__()` methods. Unlike the standard methods,
    this variant guarantees that `guarded_exit()` is called even if the
    enter operation raises an exception.
    
    Additionally, the `GuardedCM` class provides a builtin exit-stack. With
    this, you can open more context-managers from within `guarded_enter()`
    as if you would stack all of them in a `contextlib.ExitStack`.
    Similarly, this guarantees that the exit-stack is cleaned up properly
    whenever anything fails.
    
    IOW, this context-manager guarantees that whenever you do something in
    `guarded_enter()`, you will have the chance to clean it up in
    `guarded_exit()`.

--- osbuild/util/ctx.py
+++ osbuild/util/ctx.py
@@ -33,3 +33,40 @@ def suppress_oserror(*errnos):
     except OSError as e:
         if e.errno not in errnos:
             raise e
+
+
+class GuardedCM(contextlib.AbstractContextManager):
+    """Guarded Context Manager
+
+    This class implements a context-manager that calls `__exit__()` even if
+    entering the context raised an exception. Furthermore, it provides an
+    `ExitStack` as `self.exit_stack` and thus allows opening further contexts
+    and stacking them.
+
+    To use this context-manager, simply override the `guarded_enter()` and
+    `guarded_exit()` methods. You do not have to call the super-methods from
+    your functions (since they merely provide stubs).
+    """
+
+    def guarded_enter(self):
+        return self
+
+    def guarded_exit(self):
+        pass
+
+    @property
+    def guarded_stack(self):
+        return self._guarded_exit_stack
+
+    def __enter__(self):
+        self._guarded_exit_stack = contextlib.ExitStack()
+        try:
+            return self.guarded_enter()
+        except:
+            with self._guarded_exit_stack:
+                self.guarded_exit()
+            raise
+
+    def __exit__(self, exc_type, exc_value, exc_tb):
+        with self._guarded_exit_stack:
+            self.guarded_exit()
--- test/mod/test_util_ctx.py
+++ test/mod/test_util_ctx.py
@@ -41,3 +41,101 @@ class TestUtilCtx(unittest.TestCase):
                 raise OSError(errno.EPERM, "Operation not permitted")
             with ctx.suppress_oserror(errno.ENOENT, errno.ESRCH):
                 raise OSError(errno.EPERM, "Operation not permitted")
+
+    def test_guarded_cm(self):
+        #
+        # Test the `GuardedCM` context-manager utility.
+        #
+
+        # A global state object that allows controlling the context manager.
+        state = {
+            "counter": 0,
+            "enter": lambda _: None,
+            "exit": lambda _: None,
+        }
+
+        # A simple context-manager that increases the counter when entering a
+        # context, and decreases it when exiting a context. Furthermore, it
+        # calls the enter/exit callbacks from the state object.
+        class TestCM(ctx.GuardedCM):
+            _state = state
+
+            def guarded_enter(self):
+                self._state["counter"] += 1
+                return self._state["enter"](self)
+
+            def guarded_exit(self):
+                self._state["counter"] -= 1
+                return self._state["exit"](self)
+
+        # Verify entering the context calls the correct overriden methods.
+        state["enter"] = lambda _: None
+        state["exit"] = lambda _: None
+        assert state["counter"] == 0
+        with TestCM() as v:
+            assert v is None
+            assert state["counter"] == 1
+        assert state["counter"] == 0
+
+        # Verify raising an exception from within the context works as normal.
+        state["enter"] = lambda _: None
+        state["exit"] = lambda _: None
+        with self.assertRaises(SystemError):
+            assert state["counter"] == 0
+            with TestCM():
+                assert state["counter"] == 1
+                raise SystemError
+        assert state["counter"] == 0
+
+        # Verify `guarded_exit()` is called even when `guarded_enter()` raises
+        # an exception.
+        def enter_inc_raise(_self):
+            assert state["counter"] == 1
+            raise SystemError
+        state["enter"] = enter_inc_raise
+        state["exit"] = lambda _: None
+        with self.assertRaises(SystemError):
+            assert state["counter"] == 0
+            with TestCM():
+                assert False
+        assert state["counter"] == 0
+
+        # Verify the return-value is taken from `guarded_enter()`, and
+        # returning from `guarded_exit()` has no effect.
+        state["enter"] = lambda _: 71
+        state["exit"] = lambda _: True
+        assert state["counter"] == 0
+        with TestCM() as v:
+            assert v == 71
+            assert state["counter"] == 1
+        assert state["counter"] == 0
+
+        # Verify the exit-stack is correctly cleaned up.
+        def cb_dec():
+            state["counter"] -= 1
+        def enter_stacked(self):
+            state["counter"] += 2
+            self.guarded_stack.callback(cb_dec)
+            self.guarded_stack.callback(cb_dec)
+        state["enter"] = enter_stacked
+        state["exit"] = lambda _: None
+        assert state["counter"] == 0
+        with TestCM():
+            assert state["counter"] == 3
+        assert state["counter"] == 0
+
+        # Verify the exit-stack is correctly cleaned up when `guarded_enter()`
+        # raises an exception.
+        def enter_stacked_raise(self):
+            state["counter"] += 2
+            self.guarded_stack.callback(cb_dec)
+            self.guarded_stack.callback(cb_dec)
+            assert state["counter"] == 3
+            raise SystemError
+        state["enter"] = enter_stacked_raise
+        state["exit"] = lambda _: None
+        with self.assertRaises(SystemError):
+            assert state["counter"] == 0
+            with TestCM():
+                assert False
+        assert state["counter"] == 0
